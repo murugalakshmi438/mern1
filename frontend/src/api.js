3f03507b9955098ef390f584dd0a63b78e7bc0cd
murugalakshmi438 <renulakshmi.916@gmail.com>
2025-07-05 00:45:39 +0530

    Push React frontend with API connection

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // change this if your backend is hosted elsewhere
+  baseURL: 'https://mern1-uzfi.onrender.com/api', // change this if your backend is hosted elsewhere
 });
 
 export const getWeather = (city) => API.get(`/weather/${city}`);
